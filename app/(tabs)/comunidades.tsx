ae5d041f05bf9e76494eafc01aa6f7d8e0e1dc73
Sebastian <juansh2004@gmail.com>
2026-03-15 17:33:48 -0500

    refactor: reutilizar el header en todas las vistas

--- app/(tabs)/comunidades.tsx
+++ app/(tabs)/comunidades.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from "react-native";
+import Header from "@/components/header";
 
-export default function Friends() {
+export default function Comunidades() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Friends Screen</Text>
+    <View className="flex-1 bg-gray-100">
+      <Header />
+
+      <View className="flex-1 items-center justify-center">
+        <Text>Comunidades Screen</Text>
+      </View>
     </View>
   );
 }
