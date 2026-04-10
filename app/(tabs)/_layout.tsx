7fac1416f16eaad84232a90f041bf36ac88b8a49
Sebastian <juansh2004@gmail.com>
2026-04-09 23:39:21 -0500

    feat: implementar cambio de tema a vista home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FloatingNavbar } from '@/components/floating-navbar';
 import { MaterialTopTabs } from '@/components/swipable-tabs';
@@ -10,19 +10,30 @@ export default function TabsLayout() {
   const segments = useSegments();
   const current = segments[segments.length - 1];
 
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+
   const screen =
     current === 'comunidades' ? 'comunidades' : current === 'profile' ? 'profile' : 'home';
 
   return (
-    <View className="flex-1 bg-gray-50">
-      <SafeAreaView edges={['top']} className="bg-white">
-        <View className="h-14 flex-row items-center justify-between border-b border-gray-200 bg-white px-4 shadow-sm">
+    <View className={isDark ? 'flex-1 bg-black' : 'flex-1 bg-gray-50'}>
+      {/* HEADER */}
+      <SafeAreaView edges={['top']} className={isDark ? 'bg-black' : 'bg-white'}>
+        <View
+          className={
+            isDark
+              ? 'h-14 flex-row items-center justify-between border-b border-gray-800 px-4'
+              : 'h-14 flex-row items-center justify-between border-b border-gray-200 bg-white px-4 shadow-sm'
+          }
+        >
           <View />
           <HeaderActions screen={screen} />
         </View>
       </SafeAreaView>
 
-      <View className="flex-1 bg-gray-100">
+      {/* BODY */}
+      <View className={isDark ? 'flex-1 bg-gray-900' : 'flex-1 bg-gray-100'}>
         <MaterialTopTabs
           tabBar={(props) => <FloatingNavbar {...props} />}
           screenOptions={{
