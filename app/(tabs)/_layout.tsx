f52b453a0c4347c2d710107b7bc62dbc412ad38c
Sebastian <juansh2004@gmail.com>
2026-05-10 20:50:26 -0500

    fix: sincronizar header con dark mode global

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
-import { useTheme } from '@/providers/theme';
+import { View, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSegments } from 'expo-router';
+
 import { FloatingNavbar } from '@/components/floating-navbar';
 import { MaterialTopTabs } from '@/components/swipable-tabs';
 import HeaderActions from '@/components/header';
-import { useSegments } from 'expo-router';
 
 export default function TabsLayout() {
   const segments = useSegments();
+
   const current = segments[segments.length - 1];
 
-  const { theme } = useTheme();
-  const isDark = theme === 'dark';
+  // DARK MODE
+  const colorScheme = useColorScheme();
+
+  const isDark = colorScheme === 'dark';
 
   const screen =
     current === 'comunidades' ? 'comunidades' : current === 'profile' ? 'profile' : 'home';
 
   return (
-    <View className={isDark ? 'flex-1 bg-black' : 'flex-1 bg-gray-50'}>
+    <View className="flex-1 bg-gray-50 dark:bg-black">
       {/* HEADER */}
-      <SafeAreaView edges={['top']} className={isDark ? 'bg-black' : 'bg-white'}>
-        <View
-          className={
-            isDark
-              ? 'h-14 flex-row items-center justify-between border-b border-gray-800 px-4'
-              : 'h-14 flex-row items-center justify-between border-b border-gray-200 bg-white px-4 shadow-sm'
-          }
-        >
+      <SafeAreaView edges={['top']} className="bg-white dark:bg-black">
+        <View className="h-14 flex-row items-center justify-between border-b border-gray-200 bg-white px-4 dark:border-zinc-800 dark:bg-black">
           <View />
+
           <HeaderActions screen={screen} />
         </View>
       </SafeAreaView>
 
       {/* BODY */}
-      <View className={isDark ? 'flex-1 bg-gray-900' : 'flex-1 bg-gray-100'}>
+      <View className="flex-1 bg-gray-100 dark:bg-zinc-950">
         <MaterialTopTabs
           tabBar={(props) => <FloatingNavbar {...props} />}
           screenOptions={{
             tabBarShowLabel: false,
-            tabBarStyle: { height: 0, position: 'absolute', top: -100 },
+            tabBarStyle: {
+              height: 0,
+              position: 'absolute',
+              top: -100,
+            },
           }}
         >
           <MaterialTopTabs.Screen name="index" />
